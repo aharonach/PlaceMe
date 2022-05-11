9d06e30019efa7d0a0a802c55b5ce4ad8eb1711e
GalYes <galy1200@gmail.com>
2022-05-11 23:39:53 +0300

    edit pupil ant test controller

--- src/main/java/jen/example/hibernate/controller/PersonController.java
+++ src/main/java/jen/example/hibernate/controller/PersonController.java
@@ -1,11 +1,12 @@
 package jen.example.hibernate.controller;
 
-import jen.example.hibernate.entity.Person;
+import jen.example.hibernate.entity.*;
 import jen.example.hibernate.service.PersonService;
+import jen.example.hibernate.service.PupilService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -14,6 +15,37 @@ public class PersonController {
 
     private final PersonService personService;
 
+    private final PupilService pupilService;
+
+    @GetMapping("/")
+    public Pupil test() {
+
+        Pupil pupil = new Pupil();
+        pupil.setGender(Pupil.Gender.MALE);
+        pupil.setFirstName("Gal");
+        pupil.setLastName("Yeshua");
+
+//        List<AttributeValue> attributeValues = new ArrayList<>();
+//        Attribute attribute = new RangeAttribute();
+//        attribute.setName("some attribute");
+//        attribute.setDescription("some description");
+//        attribute.setPriority(10);
+//
+//        AttributeValue attributeValue = new AttributeValue();
+//        attributeValue.setAttribute(attribute);
+//        attributeValue.setPupil(pupil);
+//        attributeValue.setValue(5);
+//
+//        attributeValues.add(attributeValue);
+//
+//        pupil.setAttributeValues(attributeValues);
+
+        return pupilService.save(pupil);
+    }
+
+
+
+
     @GetMapping("/get")
     public String get() {
         List<Person> persons = personService.fetchList();
@@ -21,8 +53,9 @@ public class PersonController {
     }
 
     @GetMapping("/add")
-    public String add() {
+    public String add(@RequestParam String name) {
         Person person = new Person();
+        person.setName(name);
         Person newPerson = personService.save(person);
         return newPerson.toString();
     }
