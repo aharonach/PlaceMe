d5c4bb4082ae89d4ff4e0e8cd62f8da9a3f64818
GalYes <galy1200@gmail.com>
2022-05-19 18:19:19 +0300

    add group entity and repository

---
+++ src/main/java/jen/example/hibernate/entity/Group.java
@@ -0,0 +1,48 @@
+package jen.example.hibernate.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "groups")
+public class Group {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    @ManyToOne
+    private Template template;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<Pupil> pupils;
+
+
+    public Group(String name, String description, Template template){
+        this.name = name;
+        this.description = description;
+        this.template = template;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Group group = (Group) o;
+        return id != null && Objects.equals(id, group.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
---
+++ src/main/java/jen/example/hibernate/repository/GroupRepository.java
@@ -0,0 +1,9 @@
+package jen.example.hibernate.repository;
+
+import jen.example.hibernate.entity.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
