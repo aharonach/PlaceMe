219a6f3da9db17e0a3f29b15a9cfabff93a4731d
GalYes <galy1200@gmail.com>
2022-05-21 18:06:49 +0300

    add pupil DTO

--- src/main/java/jen/example/hibernate/assembler/PupilModelAssembler.java
+++ src/main/java/jen/example/hibernate/assembler/PupilModelAssembler.java
@@ -1,6 +1,7 @@
 package jen.example.hibernate.assembler;
 
 import jen.example.hibernate.controller.PupilRestController;
+import jen.example.hibernate.dto.PupilDto;
 import jen.example.hibernate.entity.Pupil;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -11,19 +12,19 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class PupilModelAssembler implements RepresentationModelAssembler<Pupil, EntityModel<Pupil>> {
+public class PupilModelAssembler implements RepresentationModelAssembler<Pupil, EntityModel<PupilDto>> {
     Class<PupilRestController> controller = PupilRestController.class;
 
     @Override
-    public EntityModel<Pupil> toModel(Pupil entity) {
-        return EntityModel.of(entity,
+    public EntityModel<PupilDto> toModel(Pupil entity) {
+        return EntityModel.of(new PupilDto(entity),
                 linkTo(methodOn(controller).get(entity.getId())).withSelfRel(),
                 linkTo(methodOn(controller).getAll()).withRel("pupils")
         );
     }
 
     @Override
-    public CollectionModel<EntityModel<Pupil>> toCollectionModel(Iterable<? extends Pupil> entities) {
+    public CollectionModel<EntityModel<PupilDto>> toCollectionModel(Iterable<? extends Pupil> entities) {
         return RepresentationModelAssembler.super.toCollectionModel(entities)
                 .add(linkTo(methodOn(controller).getAll()).withSelfRel());
     }
