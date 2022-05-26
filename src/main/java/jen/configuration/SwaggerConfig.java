a4300ee859e1706d44638513775e0df6f36b4029
GalYes <galy1200@gmail.com>
2022-05-26 18:17:26 +0300

    add placements endpoints to swagger

--- src/main/java/jen/configuration/SwaggerConfig.java
+++ src/main/java/jen/configuration/SwaggerConfig.java
@@ -25,6 +25,7 @@ public class SwaggerConfig {
                 .group("placeMe-public")
                 .pathsToMatch("/pupils/**")
                 .pathsToMatch("/templates/**")
+                .pathsToMatch("/placements/**")
                 .pathsToMatch("/groups/**")
                 .pathsToMatch("/**")
                 .build();
