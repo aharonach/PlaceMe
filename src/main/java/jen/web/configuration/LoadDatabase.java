536e6a0f25733b4b5a4d0bb959f6864ba8c0caf0
GalYes <galy1200@gmail.com>
2022-09-04 02:28:15 +0300

    remove data on init

--- src/main/java/jen/web/configuration/LoadDatabase.java
+++ src/main/java/jen/web/configuration/LoadDatabase.java
@@ -7,7 +7,6 @@ import jen.web.service.PupilService;
 import jen.web.service.TemplateService;
 import jen.web.util.CsvUtils;
 import jen.web.util.IsraeliIdValidator;
-import jen.web.util.PagesAndSortHandler;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -21,8 +20,6 @@ import org.springframework.context.annotation.Profile;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
-import java.time.LocalDate;
-import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
@@ -49,52 +46,8 @@ public class LoadDatabase {
             // config for alg
             createEngineConfig();
 
-            // create template, group, placement and import data
             createTemplates();
             createGroups();
-            createPlacements(); // create placement and import data from csv
-
-            // generate results
-            List<Placement> placements = placementService.allWithoutPages();
-            placementService.generatePlacementResult(placements.get(0), "name", "test 4 classes");
-//            placementService.generatePlacementResult(placements.get(1), "name", "test 6 classes");
-//            placementService.generatePlacementResult(placements.get(2), "name", "test 8 classes");
-
-//            // print
-//            System.out.println("Pupils:");
-//            pupilService.allWithoutPages().forEach(pupil -> {
-//                System.out.println(pupil);
-//                System.out.println(pupil.getAttributeValues());
-//            });
-//
-//            System.out.println("Templates:");
-//            templateService.allWithoutPages().forEach(System.out::println);
-//
-//            System.out.println("Groups:");
-//            groupService.allWithoutPages().forEach(System.out::println);
-//
-//            System.out.println("Placements:");
-//            placementService.allWithoutPages().forEach(placement -> {
-//                System.out.println(placement);
-//                System.out.println(placement.getGroup());
-//                System.out.println(placement.getGroup().getPupils());
-//            });
-//
-//            System.out.println("Prefs:");
-//            System.out.println(groupService.allWithoutPages().get(0).getPreferences());
-//
-//            System.out.println("Result:");
-//            PlacementResult placementResult = placementService.getOr404(1L).getResultById(1L);
-//            System.out.println(placementResult.getPlacementScore());
-//            System.out.println(placementResult);
-//            placementResult.getClasses().forEach(classInfo -> {
-//                System.out.print("[Pupils: " + classInfo.getNumOfPupils() + " (Males: " + classInfo.getNumOfPupilsByGender(Pupil.Gender.MALE) + " ,Females: " + classInfo.getNumOfPupilsByGender(Pupil.Gender.FEMALE) + " ,Delta: " + classInfo.getDeltaBetweenMalesAndFemales() + ") ");
-//                System.out.print("Pupils Score: " + classInfo.getSumScoreOfPupils() + " Class Score: " + classInfo.getClassScore() + " ");
-//                System.out.print("Include is OK: " + (classInfo.getNumberOfWrongConnectionsToInclude() == 0) + ", ");
-//                System.out.print("Exclude is OK: " + (classInfo.getNumberOfWrongConnectionsToExclude() == 0));
-//                System.out.print("] | ");
-//                System.out.println(classInfo.getPupils());
-//            });
         };
     }
 
@@ -120,29 +73,22 @@ public class LoadDatabase {
     }
 
     private void createPlacements() throws FileNotFoundException, CsvUtils.CsvContent.CsvNotValidException, PlacementService.PlacementWithoutTemplateInGroupException, PlacementService.PlacementWithoutGroupException {
-        Group group = groupService.getOr404(1L);
-        String content = getFileContent("placement1.csv");
-
-        Placement placement1 = placementService.add(new Placement("placement 1", 4, group));
-        System.out.println(placementService.importDataFromCsv(placement1, content).getErrors());
+        Group group1 = groupService.getOr404(1L);
+        String content1 = getFileContent("placement1.csv");
+        Placement placement1 = placementService.add(new Placement("placement 1", 4, group1));
+        System.out.println(placementService.importDataFromCsv(placement1, content1).getErrors());
         logger.info("Preloading " + placement1);
-
-//        Placement placement2 = placementService.add(new Placement("placement 2", 6, group));
-//        System.out.println(placementService.importDataFromCsv(placement2, content).getErrors());
-//        logger.info("Preloading " + placement2);
-//
-//        Placement placement3 = placementService.add(new Placement("placement 3", 8, group));
-//        System.out.println(placementService.importDataFromCsv(placement3, content).getErrors());
-//        logger.info("Preloading " + placement3);
     }
 
     private String getFileContent(String fileName) throws FileNotFoundException {
         File placementCsv1 = Paths.get("src", "main", "java", "jen", "web", "configuration", "csv", fileName).toFile();
-        Scanner myReader = new Scanner(placementCsv1);
-        StringBuilder fileContent = new StringBuilder();
-        while (myReader.hasNextLine()) {
-            fileContent.append(myReader.nextLine()).append("\n");
+
+        try(Scanner myReader = new Scanner(placementCsv1)){
+            StringBuilder fileContent = new StringBuilder();
+            while (myReader.hasNextLine()) {
+                fileContent.append(myReader.nextLine()).append("\n");
+            }
+            return fileContent.toString().trim();
         }
-        return fileContent.toString();
     }
 }
